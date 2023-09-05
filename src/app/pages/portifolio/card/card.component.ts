304d6ed579410a7b09a48e4910c821f5238027db
Evan Joseph <evandromariano49@gmail.com>
2023-09-05 20:03:21 -0300

    feat: recuperando os parametros das rotas

--- src/app/pages/portifolio/card/card.component.ts
+++ src/app/pages/portifolio/card/card.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-
+  constructor(private activeRout: ActivatedRoute) { // recupera os parâmetros das rotas
+    this.activeRout.params.subscribe();
+    (res: any) => console.log(res);
+  }
 }

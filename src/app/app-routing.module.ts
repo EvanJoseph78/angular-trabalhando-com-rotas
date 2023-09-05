357b40823c2332029bffd6409b2c335202f24c23
Evan Joseph <evandromariano49@gmail.com>
2023-09-05 19:01:26 -0300

    feat: menu-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { CardComponent } from './pages/portifolio/card/card.component';
 const routes: Routes = [
   { path: '', component: TitleComponent, pathMatch: 'full' },
   { path: 'portfolio', component: CardComponent, pathMatch: 'prefix' },
+  { path: '**', redirectTo: ''} // redireciona novamente para a home ** => qualquer coisa
 ];
 
 @NgModule({
